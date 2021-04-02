c0666c6de09428dcf49e207e0e851ab203f5fd04
Cristopher Soto <csc8848@gmail.com>
2021-04-01 21:25:27 -0600

    Firts entity, function and the creation on scr/

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,21 @@ pragma solidity ^0.5.0;
 contract  TodoList {
     // State variable = represent the state of the smart contract in the blockchain
     uint public taskCount = 0;
+
+    struct Task{
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("My first smart contract");
+    }
+
+    function createTask(string memory _content) public{
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
